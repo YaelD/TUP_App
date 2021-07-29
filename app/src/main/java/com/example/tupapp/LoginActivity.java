0a001c8adb0e01e359d7e230a0799681ad7f3bd4
Matan <matanp103@gmail.com>
2021-07-29 11:13:47 +0300

    Added Logic to Register and Login activity

--- app/src/main/java/com/example/tupapp/LoginActivity.java
+++ app/src/main/java/com/example/tupapp/LoginActivity.java
@@ -13,40 +13,45 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.material.appbar.MaterialToolbar;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private final String URL = "http://10.0.0.5/LoginRegister/login.php";
     private static final String TAG = "LoginActivity";
     private EditText txtEmail, txtPassword;
     private TextView txtWarnEmail, txtWarnPassword;
     private Button btnLoginActivity;
+    private String email, password;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
         initViews();
 
-        btnLoginActivity.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                txtWarnEmail.setVisibility(View.GONE);
-                txtWarnPassword.setVisibility(View.GONE);
-                initLogin();
-            }
-        });
     }
 
     private void initLogin(){
         Log.d(TAG, "initLogin: started");
 
         if(validateData()){
-            Intent intent= new Intent(LoginActivity.this, MainScreenActivity.class);
-            startActivity(intent);
+            getLogInInfoFromDB();
+            //Intent intent= new Intent(LoginActivity.this, MainScreenActivity.class);
+            //startActivity(intent);
         }
     }
 
@@ -73,5 +78,48 @@ public class LoginActivity extends AppCompatActivity {
         txtWarnEmail= findViewById(R.id.txtWarnEmail);
         txtWarnPassword = findViewById(R.id.txtWarnPassword);
         btnLoginActivity = findViewById(R.id.btnLoginActivity);
+    }
+
+    public void login(View view) {
+        txtWarnEmail.setVisibility(View.GONE);
+        txtWarnPassword.setVisibility(View.GONE);
+        initLogin();
+
+    }
+
+    public void getLogInInfoFromDB()
+    {
+        email = txtEmail.getText().toString().trim();
+        password = txtPassword.getText().toString().trim();
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                if (response.equals("Login Success")) {
+
+                    Intent intent = new Intent(LoginActivity.this, MainScreenActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+                else {
+                    Toast.makeText(LoginActivity.this, response, Toast.LENGTH_SHORT).show();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(LoginActivity.this, error.toString().trim(), Toast.LENGTH_SHORT).show();
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> data = new HashMap<>();
+                data.put("email", email);
+                data.put("password", password);
+                return data;
+            }
+        };
+        RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+        requestQueue.add(stringRequest);
+
     }
 }
--- app/src/main/java/com/example/tupapp/RegisterActivity.java
+++ app/src/main/java/com/example/tupapp/RegisterActivity.java
@@ -10,36 +10,40 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.material.snackbar.BaseTransientBottomBar;
 import com.google.android.material.snackbar.Snackbar;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class RegisterActivity extends AppCompatActivity {
 
+
+    private final String URL = "http://10.0.0.5/LoginRegister/signup.php";
     private static final String TAG = "RegisterActivity";
     private EditText txtFirstName, txtLastName, txtEmailAddr, txtPasswordRegister, txtRePassword;
     private TextView txtWarnFirstName, txtWarnLastName, txtWarningEmail, txtWarningPassword, txtWarningComfirmPass;
     private Button btnRegisterSystem;
     private ConstraintLayout parent;
+    private  String firstName, lastName, email, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-
+        firstName = lastName = email = password = "";
         initViews();
 
-        btnRegisterSystem.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                txtWarnFirstName.setVisibility(View.GONE);
-                txtWarnLastName.setVisibility(View.GONE);
-                txtWarningEmail.setVisibility(View.GONE);
-                txtWarningPassword.setVisibility(View.GONE);
-                txtWarningComfirmPass.setVisibility(View.GONE);
-                initRegister();
-            }
-        });
+
     }
 
     private void initRegister(){
@@ -47,9 +51,9 @@ public class RegisterActivity extends AppCompatActivity {
 
         if(validateData()){
             showSnackBar();
-            Intent intent= new Intent(RegisterActivity.this, MainScreenActivity.class);
-            startActivity(intent);
+            sendRegisterInfoToDB();
         }
+
     }
 
     private void showSnackBar() {
@@ -128,5 +132,60 @@ public class RegisterActivity extends AppCompatActivity {
 
         btnRegisterSystem= findViewById(R.id.btnRegisterSystem);
         parent = findViewById(R.id.parent);
+    }
+
+    public void register(View view) {
+        txtWarnFirstName.setVisibility(View.GONE);
+        txtWarnLastName.setVisibility(View.GONE);
+        txtWarningEmail.setVisibility(View.GONE);
+        txtWarningPassword.setVisibility(View.GONE);
+        txtWarningComfirmPass.setVisibility(View.GONE);
+
+        initRegister();
+
+    }
+
+    private void sendRegisterInfoToDB()
+    {
+        firstName = txtFirstName.getText().toString().trim();
+        lastName = txtLastName.getText().toString().trim();
+        email = txtEmailAddr.getText().toString().trim();
+        password = txtPasswordRegister.getText().toString().trim();
+        //make a new request
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                if (response.equals("Sign Up Success")) {
+                    btnRegisterSystem.setClickable(false);
+                    Intent intent= new Intent(RegisterActivity.this, MainScreenActivity.class);
+                    finish();
+                    startActivity(intent);
+
+                }
+                else
+                {
+                    Toast.makeText(RegisterActivity.this, response, Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(RegisterActivity.this, error.toString().trim(), Toast.LENGTH_SHORT).show();
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> data = new HashMap<>();
+                data.put("firstname", firstName);
+                data.put("lastname", lastName);
+                data.put("email", email);
+                data.put("password", password);
+                return data;
+            }
+        };
+        RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+        requestQueue.add(stringRequest);
+
     }
 }
