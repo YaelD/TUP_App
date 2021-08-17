e44550e8a9550d305e3313073152e0fe0ab0795f
Matan Peretz <matanp103@gmail.com>
2021-08-17 10:35:14 +0300

    UserDetailsActivity

--- app/src/main/java/LoginAndRegister/UserDetailsActivity.java
+++ app/src/main/java/LoginAndRegister/UserDetailsActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.TupApp.R;
+
 public class UserDetailsActivity extends AppCompatActivity {
 
     @Override
